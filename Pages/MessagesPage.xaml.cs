f1db71b7a5575aae6e1274e1c9c1d919bc557f33
danielledaye7 <danielle.daye@usm.edu>
2025-04-24 14:39:40 -0500

    Create MessagesPage.xaml.cs
    This is the backend for the messages page

---
+++ Pages/MessagesPage.xaml.cs
@@ -0,0 +1,22 @@
+using CanineConnect;
+
+namespace YourAppNamespace;
+
+public partial class MessagesPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+/*
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new LoginModalPage());
+    }
+
+    private async void OnCreateAccountClicked(object sender, EventArgs e)
+    {
+       await Navigation.PushModalAsync(new CreateAccountModalPage());
+    }
+}
+*/
